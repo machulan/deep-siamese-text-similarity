a0f7641b98fa54979c70e42754d70a699ee42ec3
Nik <nik.machula@yandex.ru>
2017-12-04 16:58:41 +0400

    Update to Python 3.

--- eval.py
+++ eval.py
@@ -7,6 +7,7 @@ import time
 import datetime
 from tensorflow.contrib import learn
 from input_helpers import InputHelper
+
 # Parameters
 # ==================================================
 
@@ -14,13 +15,14 @@ from input_helpers import InputHelper
 tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
 tf.flags.DEFINE_string("checkpoint_dir", "", "Checkpoint directory from training run")
 tf.flags.DEFINE_string("eval_filepath", "validation.txt0", "Evaluate on this data (Default: None)")
-tf.flags.DEFINE_string("vocab_filepath", "runs/1512222837/checkpoints/vocab", "Load training time vocabulary (Default: None)")
-tf.flags.DEFINE_string("model", "runs/1512222837/checkpoints/model-5000", "Load trained model checkpoint (Default: None)")
+tf.flags.DEFINE_string("vocab_filepath", "runs/1512222837/checkpoints/vocab",
+                       "Load training time vocabulary (Default: None)")
+tf.flags.DEFINE_string("model", "runs/1512222837/checkpoints/model-5000",
+                       "Load trained model checkpoint (Default: None)")
 
 # Misc Parameters
 tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
 tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
-
 
 FLAGS = tf.flags.FLAGS
 FLAGS._parse_flags()
@@ -29,25 +31,25 @@ for attr, value in sorted(FLAGS.__flags.items()):
     print("{}={}".format(attr.upper(), value))
 print("")
 
-if FLAGS.eval_filepath==None or FLAGS.vocab_filepath==None or FLAGS.model==None :
+if FLAGS.eval_filepath == None or FLAGS.vocab_filepath == None or FLAGS.model == None:
     print("Eval or Vocab filepaths are empty.")
     exit()
 
 # load data and map id-transform based on training time vocabulary
 inpH = InputHelper()
-x1_test,x2_test,y_test = inpH.getTestDataSet(FLAGS.eval_filepath, FLAGS.vocab_filepath, 30)
+x1_test, x2_test, y_test = inpH.getTestDataSet(FLAGS.eval_filepath, FLAGS.vocab_filepath, 30)
 
 print("\nEvaluating...\n")
 
 # Evaluation
 # ==================================================
 checkpoint_file = FLAGS.model
-print checkpoint_file
+print(checkpoint_file)
 graph = tf.Graph()
 with graph.as_default():
     session_conf = tf.ConfigProto(
-      allow_soft_placement=FLAGS.allow_soft_placement,
-      log_device_placement=FLAGS.log_device_placement)
+        allow_soft_placement=FLAGS.allow_soft_placement,
+        log_device_placement=FLAGS.log_device_placement)
     sess = tf.Session(config=session_conf)
     with sess.as_default():
         # Load the saved meta graph and restore variables
@@ -68,21 +70,23 @@ with graph.as_default():
 
         sim = graph.get_operation_by_name("accuracy/temp_sim").outputs[0]
 
-        #emb = graph.get_operation_by_name("embedding/W").outputs[0]
-        #embedded_chars = tf.nn.embedding_lookup(emb,input_x)
+        # emb = graph.get_operation_by_name("embedding/W").outputs[0]
+        # embedded_chars = tf.nn.embedding_lookup(emb,input_x)
         # Generate batches for one epoch
-        batches = inpH.batch_iter(list(zip(x1_test,x2_test,y_test)), 2*FLAGS.batch_size, 1, shuffle=False)
+        batches = inpH.batch_iter(list(zip(x1_test, x2_test, y_test)), 2 * FLAGS.batch_size, 1, shuffle=False)
         # Collect the predictions here
         all_predictions = []
-        all_d=[]
+        all_d = []
         for db in batches:
-            x1_dev_b,x2_dev_b,y_dev_b = zip(*db)
-            batch_predictions, batch_acc, batch_sim = sess.run([predictions,accuracy,sim], {input_x1: x1_dev_b, input_x2: x2_dev_b, input_y:y_dev_b, dropout_keep_prob: 1.0})
+            x1_dev_b, x2_dev_b, y_dev_b = zip(*db)
+            batch_predictions, batch_acc, batch_sim = sess.run([predictions, accuracy, sim],
+                                                               {input_x1: x1_dev_b, input_x2: x2_dev_b,
+                                                                input_y: y_dev_b, dropout_keep_prob: 1.0})
             all_predictions = np.concatenate([all_predictions, batch_predictions])
             print(batch_predictions)
             all_d = np.concatenate([all_d, batch_sim])
             print("DEV acc {}".format(batch_acc))
         for ex in all_predictions:
-            print ex 
+            print(ex)
         correct_predictions = float(np.mean(all_d == y_test))
         print("Accuracy: {:g}".format(correct_predictions))
--- train.py
+++ train.py
@@ -13,6 +13,7 @@ from siamese_network_semantic import SiameseLSTMw2v
 from tensorflow.contrib import learn
 import gzip
 from random import random
+
 # Parameters
 # ==================================================
 
@@ -21,12 +22,14 @@ tf.flags.DEFINE_boolean("is_char_based", True, "is character based syntactic sim
                                                "(default: True)")
 
 tf.flags.DEFINE_string("word2vec_model", "wiki.simple.vec", "word2vec pre-trained embeddings file (default: None)")
-tf.flags.DEFINE_string("word2vec_format", "text", "word2vec pre-trained embeddings file format (bin/text/textgz)(default: None)")
+tf.flags.DEFINE_string("word2vec_format", "text",
+                       "word2vec pre-trained embeddings file format (bin/text/textgz)(default: None)")
 
 tf.flags.DEFINE_integer("embedding_dim", 300, "Dimensionality of character embedding (default: 300)")
 tf.flags.DEFINE_float("dropout_keep_prob", 1.0, "Dropout keep probability (default: 1.0)")
 tf.flags.DEFINE_float("l2_reg_lambda", 0.0, "L2 regularizaion lambda (default: 0.0)")
-tf.flags.DEFINE_string("training_files", "person_match.train2", "training file (default: None)")  #for sentence semantic similarity use "train_snli.txt"
+tf.flags.DEFINE_string("training_files", "person_match.train2",
+                       "training file (default: None)")  # for sentence semantic similarity use "train_snli.txt"
 tf.flags.DEFINE_integer("hidden_units", 50, "Number of hidden units (default:50)")
 
 # Training parameters
@@ -45,26 +48,25 @@ for attr, value in sorted(FLAGS.__flags.items()):
     print("{}={}".format(attr.upper(), value))
 print("")
 
-if FLAGS.training_files==None:
+if FLAGS.training_files == None:
     print("Input Files List is empty. use --training_files argument.")
     exit()
 
-
-max_document_length=15
+max_document_length = 15
 inpH = InputHelper()
-train_set, dev_set, vocab_processor,sum_no_of_batches = inpH.getDataSets(FLAGS.training_files,max_document_length, 10,
-                                                                         FLAGS.batch_size, FLAGS.is_char_based)
-trainableEmbeddings=False
-if FLAGS.is_char_based==True:
+train_set, dev_set, vocab_processor, sum_no_of_batches = inpH.getDataSets(FLAGS.training_files, max_document_length, 10,
+                                                                          FLAGS.batch_size, FLAGS.is_char_based)
+trainableEmbeddings = False
+if FLAGS.is_char_based == True:
     FLAGS.word2vec_model = False
 else:
-    if FLAGS.word2vec_model==None:
-        trainableEmbeddings=True
+    if FLAGS.word2vec_model == None:
+        trainableEmbeddings = True
         print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
-          "You are using word embedding based semantic similarity but "
-          "word2vec model path is empty. It is Recommended to use  --word2vec_model  argument. "
-          "Otherwise now the code is automatically trying to learn embedding values (may not help in accuracy)"
-          "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
+              "You are using word embedding based semantic similarity but "
+              "word2vec model path is empty. It is Recommended to use  --word2vec_model  argument. "
+              "Otherwise now the code is automatically trying to learn embedding values (may not help in accuracy)"
+              "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
     else:
         inpH.loadW2V(FLAGS.word2vec_model, FLAGS.word2vec_format)
 
@@ -73,8 +75,8 @@ else:
 print("starting graph def")
 with tf.Graph().as_default():
     session_conf = tf.ConfigProto(
-      allow_soft_placement=FLAGS.allow_soft_placement,
-      log_device_placement=FLAGS.log_device_placement)
+        allow_soft_placement=FLAGS.allow_soft_placement,
+        log_device_placement=FLAGS.log_device_placement)
     sess = tf.Session(config=session_conf)
     print("started session")
     with sess.as_default():
@@ -101,8 +103,8 @@ with tf.Graph().as_default():
         global_step = tf.Variable(0, name="global_step", trainable=False)
         optimizer = tf.train.AdamOptimizer(1e-3)
         print("initialized siameseModel object")
-    
-    grads_and_vars=optimizer.compute_gradients(siameseModel.loss)
+
+    grads_and_vars = optimizer.compute_gradients(siameseModel.loss)
     tr_op_set = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
     print("defined training_ops")
     # Keep track of gradient values and sparsity (optional)
@@ -146,43 +148,44 @@ with tf.Graph().as_default():
 
     # Initialize all variables
     sess.run(tf.global_variables_initializer())
-    
+
     print("init all variables")
     graph_def = tf.get_default_graph().as_graph_def()
     graphpb_txt = str(graph_def)
     with open(os.path.join(checkpoint_dir, "graphpb.txt"), 'w') as f:
         f.write(graphpb_txt)
 
-    if FLAGS.word2vec_model :
+    if FLAGS.word2vec_model:
         # initial matrix with random uniform
-        initW = np.random.uniform(-0.25,0.25,(len(vocab_processor.vocabulary_), FLAGS.embedding_dim))
-        #initW = np.zeros(shape=(len(vocab_processor.vocabulary_), FLAGS.embedding_dim))
+        initW = np.random.uniform(-0.25, 0.25, (len(vocab_processor.vocabulary_), FLAGS.embedding_dim))
+        # initW = np.zeros(shape=(len(vocab_processor.vocabulary_), FLAGS.embedding_dim))
         # load any vectors from the word2vec
         print("initializing initW with pre-trained word2vec embeddings")
         for w in vocab_processor.vocabulary_._mapping:
-            arr=[]
+            arr = []
             s = re.sub('[^0-9a-zA-Z]+', '', w)
             if w in inpH.pre_emb:
-                arr=inpH.pre_emb[w]
+                arr = inpH.pre_emb[w]
             elif w.lower() in inpH.pre_emb:
-                arr=inpH.pre_emb[w.lower()]
+                arr = inpH.pre_emb[w.lower()]
             elif s in inpH.pre_emb:
-                arr=inpH.pre_emb[s]
+                arr = inpH.pre_emb[s]
             elif s.isdigit():
-                arr=inpH.pre_emb["zero"]
-            if len(arr)>0:
+                arr = inpH.pre_emb["zero"]
+            if len(arr) > 0:
                 idx = vocab_processor.vocabulary_.get(w)
-                initW[idx]=np.asarray(arr).astype(np.float32)
-        print("Done assigning intiW. len="+str(len(initW)))
+                initW[idx] = np.asarray(arr).astype(np.float32)
+        print("Done assigning intiW. len=" + str(len(initW)))
         inpH.deletePreEmb()
         gc.collect()
         sess.run(siameseModel.W.assign(initW))
+
 
     def train_step(x1_batch, x2_batch, y_batch):
         """
         A single training step
         """
-        if random()>0.5:
+        if random() > 0.5:
             feed_dict = {
                 siameseModel.input_x1: x1_batch,
                 siameseModel.input_x2: x2_batch,
@@ -196,17 +199,20 @@ with tf.Graph().as_default():
                 siameseModel.input_y: y_batch,
                 siameseModel.dropout_keep_prob: FLAGS.dropout_keep_prob,
             }
-        _, step, loss, accuracy, dist, sim, summaries = sess.run([tr_op_set, global_step, siameseModel.loss, siameseModel.accuracy, siameseModel.distance, siameseModel.temp_sim, train_summary_op],  feed_dict)
+        _, step, loss, accuracy, dist, sim, summaries = sess.run(
+            [tr_op_set, global_step, siameseModel.loss, siameseModel.accuracy, siameseModel.distance,
+             siameseModel.temp_sim, train_summary_op], feed_dict)
         time_str = datetime.datetime.now().isoformat()
         print("TRAIN {}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
         train_summary_writer.add_summary(summaries, step)
         print(y_batch, dist, sim)
 
+
     def dev_step(x1_batch, x2_batch, y_batch):
         """
         A single training step
-        """ 
-        if random()>0.5:
+        """
+        if random() > 0.5:
             feed_dict = {
                 siameseModel.input_x1: x1_batch,
                 siameseModel.input_x2: x2_batch,
@@ -220,37 +226,39 @@ with tf.Graph().as_default():
                 siameseModel.input_y: y_batch,
                 siameseModel.dropout_keep_prob: 1.0,
             }
-        step, loss, accuracy, sim, summaries = sess.run([global_step, siameseModel.loss, siameseModel.accuracy, siameseModel.temp_sim, dev_summary_op],  feed_dict)
+        step, loss, accuracy, sim, summaries = sess.run(
+            [global_step, siameseModel.loss, siameseModel.accuracy, siameseModel.temp_sim, dev_summary_op], feed_dict)
         time_str = datetime.datetime.now().isoformat()
         print("DEV {}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
         dev_summary_writer.add_summary(summaries, step)
-        print (y_batch, sim)
+        print(y_batch, sim)
         return accuracy
 
-    # Generate batches
-    batches=inpH.batch_iter(
-                list(zip(train_set[0], train_set[1], train_set[2])), FLAGS.batch_size, FLAGS.num_epochs)
 
-    ptr=0
-    max_validation_acc=0.0
-    for nn in xrange(sum_no_of_batches*FLAGS.num_epochs):
-        batch = batches.next()
-        if len(batch)<1:
+    # Generate batches
+    batches = inpH.batch_iter(
+        list(zip(train_set[0], train_set[1], train_set[2])), FLAGS.batch_size, FLAGS.num_epochs)
+
+    ptr = 0
+    max_validation_acc = 0.0
+    for nn in range(sum_no_of_batches * FLAGS.num_epochs):
+        batch = next(batches)
+        if len(batch) < 1:
             continue
-        x1_batch,x2_batch, y_batch = zip(*batch)
-        if len(y_batch)<1:
+        x1_batch, x2_batch, y_batch = zip(*batch)
+        if len(y_batch) < 1:
             continue
         train_step(x1_batch, x2_batch, y_batch)
         current_step = tf.train.global_step(sess, global_step)
-        sum_acc=0.0
+        sum_acc = 0.0
         if current_step % FLAGS.evaluate_every == 0:
             print("\nEvaluation:")
-            dev_batches = inpH.batch_iter(list(zip(dev_set[0],dev_set[1],dev_set[2])), FLAGS.batch_size, 1)
+            dev_batches = inpH.batch_iter(list(zip(dev_set[0], dev_set[1], dev_set[2])), FLAGS.batch_size, 1)
             for db in dev_batches:
-                if len(db)<1:
+                if len(db) < 1:
                     continue
-                x1_dev_b,x2_dev_b,y_dev_b = zip(*db)
-                if len(y_dev_b)<1:
+                x1_dev_b, x2_dev_b, y_dev_b = zip(*db)
+                if len(y_dev_b) < 1:
                     continue
                 acc = dev_step(x1_dev_b, x2_dev_b, y_dev_b)
                 sum_acc = sum_acc + acc
@@ -259,5 +267,7 @@ with tf.Graph().as_default():
             if sum_acc >= max_validation_acc:
                 max_validation_acc = sum_acc
                 saver.save(sess, checkpoint_prefix, global_step=current_step)
-                tf.train.write_graph(sess.graph.as_graph_def(), checkpoint_prefix, "graph"+str(nn)+".pb", as_text=False)
-                print("Saved model {} with sum_accuracy={} checkpoint to {}\n".format(nn, max_validation_acc, checkpoint_prefix))
+                tf.train.write_graph(sess.graph.as_graph_def(), checkpoint_prefix, "graph" + str(nn) + ".pb",
+                                     as_text=False)
+                print("Saved model {} with sum_accuracy={} checkpoint to {}\n".format(nn, max_validation_acc,
+                                                                                      checkpoint_prefix))
